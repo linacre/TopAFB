493fd62a0331b002d8e9ea8867005376ad224be5
gutsche <>
2013-05-10 20:02:58 +0000

    changed from log to linear in Y axis

--- macro_drawHists.C
+++ macro_drawHists.C
@@ -1,4 +1,4 @@
 {
 gROOT->ProcessLine(".L drawHists.C+");
-makePSFile("results/hist_usePtGt2020_hypDisamb_usepfMET_usepfJets_useOS_vetoHypMassLt12_requireBTag_sortJetCandidatesbyPt_generalLeptonVeto_createBabyNtuples_applylepIDCuts_applylepIsoCuts_vetoZmass_veto2Jets_vetoMET.root",5,0,"default",4.98,1,1,1);
+makePSFile("results/hist_usePtGt2020_hypDisamb_usepfMET_usepfJets_useOS_vetoHypMassLt12_requireBTag_sortJetCandidatesbyPt_generalLeptonVeto_createBabyNtuples_applylepIDCuts_applylepIsoCuts_vetoZmass_veto2Jets_vetoMET.root",5,0,"default",4.98,0,1,1);
 }
