a7e086b1c47d161a69c6e1b7720c0c490de926d0
gutsche <>
2013-06-03 07:00:22 +0000

    changed treatment of acceptance plot generation, don't hardcode root file from doAll.C, get first hists_*.root file from results directory excluding hists_noCuts.root

--- doAll.sh
+++ doAll.sh
@@ -12,8 +12,12 @@ rootb -b -q macro_drawHists.C >& macro_drawHists.log
 echo "executing doAll_nocuts.C"
 root -b -q doAll_nocuts.C >& doAll_nocuts.log
 
+echo "determining histogram root file from doAll.C, using first to find in results/ except hist_noCuts.root"
+rootfile=`ls -1 results/*.root | grep -v hist_noCuts.root | head -1 | sed -e 's/results\///g'`
+echo "histogram root file: $rootfile"
+
 echo "create acceptance histograms"
-root -b -q macro_acceptanceHists.C >& macro_acceptanceHists.log
+root -b -q 'macro_acceptanceHists.C("'$rootfile'")' >& macro_acceptanceHists.log
 mkdir -p acceptance/mcnlo/
 mv -f accept_*.* acceptance/mcnlo/
 
@@ -24,7 +28,3 @@ root -b -q macro_1DUnfolding.C >& macro_1DUnfolding.log
 
 echo "run 2D unfolding"
 root -b -q macro_2DUnfolding.C >& macro_2DUnfolding.log
-
-
-
-
