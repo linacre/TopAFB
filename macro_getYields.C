c2dc48e6842b7ddce5abc7a5f7566fc9dd71c63c
linacre <>
2013-06-21 20:59:12 +0000

    fix typo

--- macro_getYields.C
+++ macro_getYields.C
@@ -1,5 +1,5 @@
 void macro_getYields(const char* rootfile){
 gROOT->ProcessLine(".L getYields.C+");
 getYields(Form("results/%s",rootfile),1.0,false,"%6.2f",false);
-getYields(Form("results/%s",1.0,false,"%6.2f",true);
+getYields(Form("results/%s",rootfile),1.0,false,"%6.2f",true);
 }
