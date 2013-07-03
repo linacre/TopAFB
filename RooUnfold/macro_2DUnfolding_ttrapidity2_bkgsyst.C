a38d5e86e12cf22040531326c692e5fb9f842785
linacre <>
2013-07-03 22:36:15 +0000

    100% syst for ttotr

--- RooUnfold/macro_2DUnfolding_ttrapidity2_bkgsyst.C
+++ RooUnfold/macro_2DUnfolding_ttrapidity2_bkgsyst.C
@@ -1,7 +1,7 @@
 {
 gROOT->ProcessLine(".L examples/AfbFinal2DUnfoldttrapidity2.cxx");
-AfbUnfoldExample(1.,1.5,1.,1.,1.,1.);
-AfbUnfoldExample(1.,0.5,1.,1.,1.,1.);
+AfbUnfoldExample(1.,2.,1.,1.,1.,1.);
+AfbUnfoldExample(1.,0.,1.,1.,1.,1.);
 AfbUnfoldExample(1.,1.,2.,1.,1.,1.);
 AfbUnfoldExample(1.,1.,0.,1.,1.,1.);
 AfbUnfoldExample(1.,1.,1.,2.,1.,1.);
