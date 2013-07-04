089f7298d7d86e79982f057dbd296b220b5913a6
linacre <>
2013-07-04 18:33:48 +0000

    update to 100% for ttotr

--- RooUnfold/cat_bkg_syst.sh
+++ RooUnfold/cat_bkg_syst.sh
@@ -6,7 +6,7 @@ done
 paste *_*_10_10_10_10_formated.txt.temp *_10_*_10_10_10_formated.txt.temp *_10_10_*_10_10_formated.txt.temp *_10_10_10_*_10_formated.txt.temp *_10_10_10_10_*_formated.txt.temp > output_1D
 rm *.temp
 
-for file in summary_2Dunfolding_formated.txt summary_2Dunfolding_1*formated.txt summary_2Dunfolding_5*formated.txt
+for file in summary_2Dunfolding_formated.txt summary_2Dunfolding_1*formated.txt summary_2Dunfolding_0*formated.txt summary_2Dunfolding_2*formated.txt
 do
         cat $file | awk '{print $4"\t"$6}' > ${file}.temp
 done
