0b768ad9da014f22042c47e486897724991f494d
linacre <>
2013-07-27 15:26:26 +0000

    run macros in parallel

--- doAll.sh
+++ doAll.sh
@@ -8,19 +8,19 @@ rootfile=`ls -1 results/*.root | grep -v hist_noCuts.root | head -1 | sed -e 's/
 echo "histogram root file: $rootfile"
 
 echo "extracting yields"
-root -b -q 'macro_getYields.C("'$rootfile'")' >& macro_getYields.log
+root -b -q 'macro_getYields.C("'$rootfile'")' >& macro_getYields.log &
 
 echo "produce plots"
-root -b -q 'macro_drawHists.C("'$rootfile'")' >& macro_drawHists.log
+root -b -q 'macro_drawHists.C("'$rootfile'")' >& macro_drawHists.log &
 
 echo "produce reco level plots"
-root -b -q macro_recoLevel.C >& macro_recoLevel.log
-root -b -q macro_2DrecoLevel.C >& macro_2DrecoLevel.log
-root -b -q macro_2DrecoLevelttpt.C >& macro_2DrecoLevelttpt.log
-root -b -q macro_2DrecoLevelttrapidity2.C >& macro_2DrecoLevelttrapidity2.log
+root -b -q macro_recoLevel.C >& macro_recoLevel.log &
+root -b -q macro_2DrecoLevel.C >& macro_2DrecoLevel.log &
+root -b -q macro_2DrecoLevelttpt.C >& macro_2DrecoLevelttpt.log &
+root -b -q macro_2DrecoLevelttrapidity2.C >& macro_2DrecoLevelttrapidity2.log &
 
 echo "executing doAll_nocuts.C"
-root -b -q doAll_nocuts.C >& doAll_nocuts.log
+root -b -q doAll_nocuts.C >& doAll_nocuts.log 
 
 echo "create acceptance histograms"
 root -b -q 'macro_acceptanceHists.C("'$rootfile'")' >& macro_acceptanceHists.log
@@ -30,13 +30,13 @@ mv -f accept_*.* acceptance/mcnlo/
 echo "run 1D unfolding"
 cd RooUnfold
 make
-root -b -q macro_1DUnfolding.C >& macro_1DUnfolding.log
+root -b -q macro_1DUnfolding.C >& macro_1DUnfolding.log &
 
 echo "run 2D unfolding"
-root -b -q macro_2DUnfolding.C >& macro_2DUnfolding.log
+root -b -q macro_2DUnfolding.C >& macro_2DUnfolding.log &
 
 echo "run 2D unfolding ttpt"
-root -b -q macro_2DUnfolding_ttpt.C >& macro_2DUnfolding_ttpt.log
+root -b -q macro_2DUnfolding_ttpt.C >& macro_2DUnfolding_ttpt.log &
 
 echo "run 2D unfolding ttrapidity2"
-root -b -q macro_2DUnfolding_ttrapidity2.C >& macro_2DUnfolding_ttrapidity2.log
+root -b -q macro_2DUnfolding_ttrapidity2.C >& macro_2DUnfolding_ttrapidity2.log &
