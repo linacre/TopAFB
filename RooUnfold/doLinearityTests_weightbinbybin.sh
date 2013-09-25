0aeb5eaa69c4a75b25acdac936bc142e6fa234bd
linacre <>
2013-09-25 20:04:31 +0000

    macro to run linearty tests weighting only a single reco bin

---
+++ RooUnfold/doLinearityTests_weightbinbybin.sh
@@ -0,0 +1,6 @@
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",1)' &
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",2)' &
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",3)' &
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",4)' &
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",5)' &
+root -q -b 'examples/AfbUnfoldTests_weightbinbybin.cxx(1,"Linearity",6)' &
